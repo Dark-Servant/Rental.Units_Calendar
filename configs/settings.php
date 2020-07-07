4892820a408aa18e52f030526c61ed556f95c4de
Konstantin Beloglazov <bk@infoservice.ru>
2020-07-07 17:17:06 +0500

    RU-34 Поправлены названия файлов для моделей БД

--- configs/settings.php
+++ configs/settings.php
@@ -1,14 +1,14 @@
 <?
 use Symfony\Component\Yaml\Yaml;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once __DIR__ . '/consts.php';
-require_once __DIR__ . '/../lang/' . LANG . '.php';
+require_once dirname(__DIR__) . '/lang/' . LANG . '.php';
 
 ActiveRecord\Config::initialize(
     function($cfg) {
         $dbSettings = Yaml::parseFile(__DIR__ . '/.php-database-migration/environments/' . ENV_CODE . '.yml');
-        $cfg->set_model_directory(__DIR__ . '/../lib/models');
+        $cfg->set_model_directory(dirname(__DIR__) . '/lib/models');
         $connection = $dbSettings['connection']['driver'] . '://'
                     . $dbSettings['connection']['username'] . ':'
                     . $dbSettings['connection']['password'] . '@'

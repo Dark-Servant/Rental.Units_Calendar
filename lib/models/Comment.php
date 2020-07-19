2cc34b69cce7309098b2bb0b35962673394f45c3
Konstantin Beloglazov <bk@infoservice.ru>
2020-07-19 02:37:32 +0000

    RU-31 Удалены lib/models/Comment.php, lib/models/Content.php, lib/models/Technic.php

--- lib/models/Comment.php
+++
@@ -1,9 +0,0 @@
-<?
-
-class Comment extends InfoserviceModel
-{
-    static $belongs_to = [
-        ['user', 'foreign_key' => 'user_id', 'class_name' => 'Responsible'],
-        ['content'],
-    ];
-};

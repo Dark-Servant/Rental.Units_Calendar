eb706224e0e2616e17fb8eced39dd47d767a0e62
Konstantin Beloglazov <bk@infoservice.ru>
2024-02-06 15:04:21 +0500

    WEB-3066 Добавлен класс Bugs\Models\Solutions\Dublicates\Data\Contents\Base для поиска дубликатов среди контента

---
+++ lib/helpers/bugs/models/solutions/dublicates/data/contents/base.class.php
@@ -0,0 +1,49 @@
+<?
+namespace Bugs\Models\Solutions\Dublicates\Data\Contents;
+
+use Bugs\Models\Solutions\Dublicates\Data\Base as BaseDublicates;
+use SQL\FileByLastFolderIniting;
+
+class Base extends BaseDublicates
+{
+    use FileByLastFolderIniting;
+
+    protected $partOfOriginal = [];
+
+    public function getDataViaQuery(): \PDOStatement
+    {
+        return $this->sqlCode->queryGetAllDublicates();
+    }
+
+    public function checkRequiringOfWorkWithNextRecord(array $record): bool
+    {
+        if (
+            !isset($this->partOfOriginal[$record['specification_id']])
+            && (
+                !isset($this->originalCodeIDs[$record['specification_id']])
+                || ($this->originalCodeIDs[$record['specification_id']] == $record['id'])
+            )
+        ) $this->partOfOriginal[$record['specification_id']] = $record['id'];
+
+        return (
+            !isset($this->partOfOriginal[$record['specification_id']])
+            || ($this->partOfOriginal[$record['specification_id']] == $record['id'])
+            || ($this->partOfOriginal[$record['specification_id']] != $record['sort'])
+        );
+    }
+
+    public static function getModelName(): string
+    {
+        return \Content::class;
+    }
+
+    public static function getGroupCodeByRecord(array $record): string
+    {
+        return $record['specification_id'];
+    }
+
+    public function checkAsOriginalAtRecordForGroupData(array $record, array $groupData = null): bool
+    {
+        return !isset($this->originalCodeIDs[$record['specification_id']]) && empty($groupData['ID']);
+    }
+}

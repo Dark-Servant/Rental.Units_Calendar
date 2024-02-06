442a99eeb5d668b6adb0f6d34cba16858e9f79bc
Konstantin Beloglazov <bk@infoservice.ru>
2024-02-06 14:43:03 +0500

    WEB-3066 С учетом добавленных классов поправлен код, в котором запускается поиск комментариев с неправильно указанными контентом и/или техникой

--- bugs/comments/badhosts/index.php
+++ bugs/comments/badhosts/index.php
@@ -1,32 +1,37 @@
 <?
-$setting = require __DIR__ . '/../../../configs/settings.php';
+use Bugs\Models\Solutions\Badhost\Comments\{
+	Log\Base as Outer,
+	Base as BadHost
+};
+use SQL\Query as Query;
+use Log\Base as Logger;
 
-use Bugs\Comments\BadHost;
+require_once __DIR__ . '/../../../configs/settings.php';
 
-require_once __DIR__ . '/outtestdata.php';
+if (!defined('NOT_CHANGE_COMMENT_HOST')) define('NOT_CHANGE_COMMENT_HOST', false);
 
+$mainLogger = Logger::getMainInstance();
+Logger::setMainInstance($logger = new Logger(__DIR__ . '/log'));
+$logger->addRightsForClassNames(
+    Query::class,
+    Outer::class
+);
 
 $badHost = new BadHost;
-$badHost->initMyTechnicData()->initPartnerData();
+$outer = new Outer($badHost);
+if ($outer->getFullCommentCount()) {
+	$outer->sendToLoggerDataForZero()
+		  ->sendToLoggerDataForMy()
+		  ->sendToLoggerDataForPartner()
+		  ->sendToLoggerAllCommentCount();
 
-echo '*********** ZERO COMMENTS:' . PHP_EOL;
-print_r(createDataForZero($badHost->getZeroContentCommentIDs()));
+	if (NOT_CHANGE_COMMENT_HOST) Query::activateSending(false);
 
-echo '*********** MY TECHNIC DATA:' . PHP_EOL;
-print_r(createDataForMy($badHost->getMyTechnicContentCommentIDs()));
+	$badHost->updateZeroContentComments()->updateMyTechnicComments()->updatePartnerComments();
 
-echo '*********** PARTNER DATA:' . PHP_EOL;
-print_r(createDataForPartner($badHost->getPartnerContentCommentIDs()));
+	Query::activateSending();
 
-$commentCount = count($badHost->getZeroContentCommentIDs());
-foreach ($badHost->getMyTechnicContentCommentIDs() as $commentIDs) {
-	$commentCount += count($commentIDs);
+} else {
+	$logger->deleteFilePath();
 }
-foreach ($badHost->getPartnerContentCommentIDs() as $technicCommentIDs) {
-	foreach ($technicCommentIDs as $commentIDs) {
-	        $commentCount += count($commentIDs);
-	}
-}
-echo 'COMMENT COUNT: ' . $commentCount . PHP_EOL;
-
-//$badHost->updateZeroContentComments()->updateMyTechnicComments()->updatePartnerComments();
+Logger::setMainInstance($mainLogger);

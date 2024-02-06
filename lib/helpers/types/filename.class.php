bc4796caf4c720cd004523fe7f0544856fbe01d6
Konstantin Beloglazov <bk@infoservice.ru>
2024-02-06 05:31:26 +0500

    WEB-3066 В класс Types\FileName добавлен метод для получения имени файла без расширения

--- lib/helpers/types/filename.class.php
+++ lib/helpers/types/filename.class.php
@@ -8,15 +8,36 @@ class FileName
 {
     protected $fullValue;
     protected $value;
-    protected $extentionValue;
+    protected $nameValue = '';
+    protected $extentionValue = '';
 
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     */
     public function __construct(string $name)
+    {
+        $this->prepareData($name);
+    }
+    
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     * @return self
+     */
+    protected function prepareData(string $name): self
     {
         $this->fullValue = strtolower(trim(preg_replace('/\?.*$/', '', $name), '\\/'));
         $this->value = preg_match('/([^\/\\\\]+)$/i', $this->fullValue, $nameParts) ? $nameParts[1] : '';
-        $this->extentionValue = preg_match('/\.(\w+)$/i', $this->value, $nameParts) ? $nameParts[1] : '';
+        if (preg_match('/(.+?)(?:\.(\w+))?$/i', $this->value, $nameParts)) {
+            $this->nameValue = $nameParts[1];
+            $this->extentionValue = $nameParts[2] ?? '';
+        }
+        return  $this;
     }
-    
+
     /**
      * Undocumented function
      *
@@ -35,6 +56,16 @@ class FileName
     public function getFullValue(): string
     {
         return $this->fullValue;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return string
+     */
+    public function getNameValue(): string
+    {
+        return $this->nameValue;
     }
 
     /**
